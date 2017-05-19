ce0baab2e77636bd7ed6a9c6e068e4ee30f3ca18
Thibaud <thibaud@we-studio.ch>
2017-05-19 18:57:34 +0200

    Carousel options

--- app/public/wp-content/themes/ws-starter-theme/src/constants.php
+++ app/public/wp-content/themes/ws-starter-theme/src/constants.php
@@ -3,4 +3,4 @@
 define('WB_VERSION',        '1.4.2');
 define('WB_LINKS_ENABLED',  false);
 define('WB_PATH',           dirname(dirname(__FILE__)));
-define('WB_EXCERPT_LENGTH', get_field( 'constants_excerpt_length', 'option' ));
+define('WB_EXCERPT_LENGTH', ( get_field( 'constants_excerpt_length', 'option' ) ?: 50 ) );
