a60ef3ee8dd2712dc782019ee19f4867f63c6b14
Thibaud Alt <thibaudalt@users.noreply.github.com>
2017-10-11 15:51:07 +0200

    Update functions.php

--- app/public/wp-content/themes/ws-starter-theme/functions.php
+++ app/public/wp-content/themes/ws-starter-theme/functions.php
@@ -1,8 +1,8 @@
 <?php
 
+require_once dirname(__FILE__) . '/src/helpers.php';
 require_once dirname(__FILE__) . '/src/acf.php';
 require_once dirname(__FILE__) . '/src/constants.php';
-require_once dirname(__FILE__) . '/src/helpers.php';
 require_once dirname(__FILE__) . '/src/settings.php';
 require_once dirname(__FILE__) . '/src/admin.php';
 require_once dirname(__FILE__) . '/src/actions.php';
