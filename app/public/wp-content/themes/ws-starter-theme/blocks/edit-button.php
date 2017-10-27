00f7a924ed0d9373e4de9c2a79cd5b13413ad6c4
Thibaud Alt <thibaudalt@users.noreply.github.com>
2017-10-27 11:00:48 +0200

    Update edit-button.php

--- app/public/wp-content/themes/ws-starter-theme/blocks/edit-button.php
+++ app/public/wp-content/themes/ws-starter-theme/blocks/edit-button.php
@@ -1,3 +1,5 @@
+<?php if ( !is_user_administrator() ) return; ?>
+
 <a href="<?php echo get_edit_post_link( get_the_ID() ) ?>" class="mt-2 btn btn-primary btn-sm pull-right" title="Edit">
   <i class="fa fa-pencil" aria-hidden="true"></i>
 </a>
