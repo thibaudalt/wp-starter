a5038ca5181e72cb9360fb13098ca6ff9f08e9d3
Thibaud <thibaud.alt@gmail.com>
2017-12-22 14:11:36 +0100

    Buttons :b:

--- app/public/wp-content/themes/ws-starter-theme/blocks/edit-button.php
+++ app/public/wp-content/themes/ws-starter-theme/blocks/edit-button.php
@@ -1,5 +1,5 @@
 <?php if ( !is_user_administrator() ) return; ?>
 
-<a href="<?php echo get_edit_post_link( get_the_ID() ) ?>" class="mt-2 btn btn-primary btn-sm pull-right" title="Edit">
-  <i class="fa fa-pencil" aria-hidden="true"></i>
+<a href="<?php echo get_edit_post_link( get_the_ID() ) ?>" class="btn btn-primary btn-sm btn-edit float-right" title="Edit">
+  <span class="edit"></span>
 </a>
