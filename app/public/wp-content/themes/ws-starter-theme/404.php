89147c5e1c3471273215ca77469681eaf86141f7
Thibaud <thibaud.alt@gmail.com>
2018-11-30 15:53:39 +0100

    Update jQuery

--- app/public/wp-content/themes/ws-starter-theme/404.php
+++ app/public/wp-content/themes/ws-starter-theme/404.php
@@ -7,12 +7,12 @@
         <div class="page 404">
 
           <div class="page-header">
-            <h1 class="page-title"><?php _e( 'Page not found', 'ws-starter' ); ?></h1>
+            <h1 class="page-title">404 - <?php _e( 'Page not found', 'ws-starter' ); ?></h1>
           </div><!-- .page-header -->
 
           <div class="page-body">
             <p><?php _e( 'It seems than what you\'re looking for is no longer here.', 'ws-starter' ); ?></p>
-            <a href="<?php echo home_url() ?>" title="<?php _e( 'Back to the homepage', 'ws-starter' ); ?>" rel="home"><?php _e( 'Back to the homepage', 'ws-starter' ); ?></a>
+            <a href="<?php echo home_url() ?>" title="<?php _e( 'Back to homepage', 'ws-starter' ); ?>" rel="home"><?php _e( 'Back to the homepage', 'ws-starter' ); ?></a>
           </div><!-- .page-body -->
 
         </div><!-- .page -->
